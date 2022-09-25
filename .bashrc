8d3b1d5ab3d1b73148c19f17f5f3f5d9bf0e54bc
Belmin Muslimovic <35530176+iambelmin@users.noreply.github.com>
2022-09-25 02:07:52 +0200

    Changes'

--- .bashrc
+++ .bashrc
@@ -202,3 +202,8 @@ source "$BASH_IT"/bash_it.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+export TERM=xterm-256color
+export PATH=$PATH:/usr/local/go/bin
+export PATH=$PATH:/home/iambelmin/go/bin
+export PATH=/usr/local/bin/nvim/bin/:$PATH
---
+++ .config/nvim/nvim/lua/iambelmin/options.lua
@@ -0,0 +1,36 @@
+local options = {
+	cmdheight = 2,
+	completeopt = { "menuone", "noselect" },
+  smarttab = true,
+	conceallevel = 0,
+	fileencoding = "utf-8",
+  tabstop = 2,
+  shiftwidth = 2,
+	pumheight = 10,
+	smartcase = true,
+	smartindent = true,
+	splitbelow = true,
+	splitright = true,
+  clipboard = "unnamedplus",
+	undofile = true,
+	updatetime = 300,
+	writebackup = false,
+	expandtab = true,
+	cursorline = true,
+	numberwidth = 2,
+  cc = "120",
+	showmode = true,
+	showtabline = 2,
+  signcolumn = "yes",
+  swapfile = false,
+  relativenumber = true,
+  number = true,
+  ignorecase = true,
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+--vim.cmd "highlight clear SignColumn"
+
+vim.cmd "set shortmess+=c"
